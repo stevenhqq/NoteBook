36ae3a8897b707b8161b178f75fc6e83630b25d2
stevenhqq <steven.hqq@163.com>
2020-04-25 16:47:48 +0800

    新建项目

---
+++ HQQNoteBook-2019/HQQNoteBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HQQNoteBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
